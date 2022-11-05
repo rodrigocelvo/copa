be45f92d729845157b26d77fa569319f8297d725
Rodrigo Celvo <rodrigo.pc.12@gmail.com>
2022-11-05 14:23:59 -0300

    first commit

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Center, VStack, Text } from "native-base";
+
+export function SignIn() {
+  return (
+    <Center flex={1} bgColor="gray.950">
+      <Text color="white">SignIn</Text>
+    </Center>
+  );
+}
