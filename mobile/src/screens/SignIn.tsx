feb942bb4bc69c9085b29c661aeaea826f317500
Rodrigo Celvo <rodrigo.pc.12@gmail.com>
2022-11-05 20:26:34 -0300

    chore: finished interface signin

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,10 +1,25 @@
-import React from "react";
-import { Center, VStack, Text } from "native-base";
+import React from 'react';
+import { Center, VStack, Text, Icon } from 'native-base';
+import { Fontisto } from '@expo/vector-icons';
+
+import Logo from '../assets/logo.svg';
+import { Button } from '../components/Button';
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor="gray.950">
-      <Text color="white">SignIn</Text>
+    <Center flex={1} bgColor="gray.950" p={7}>
+      <Logo width={212} height={40} />
+      <Button
+        type="SECONDARY"
+        title="Entrar com o Google"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        mt={12}
+      />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'}
+        do seu e-mail para a criação de sua conta.
+      </Text>
     </Center>
   );
 }
